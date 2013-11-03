b9d9fac35d785b84f8bb2f236edca379a553f538
sidharthm <sidhartm@usc.edu>
2013-11-02 18:09:07 -0700

    Renamed tests to be more descriptive, added third cashier test.

--- src/restaurant/test/CashierTest.java
+++ src/restaurant/test/CashierTest.java
@@ -122,7 +122,7 @@ public class CashierTest extends TestCase
 
 	}//end two normal market scenarios
 	
-	public void testNormalRestaurantScenario(){
+	public void testSingleCustomerScenario(){
 		//This test has the cashier deal with a single customer paying for his meal in full.
 		//setUp() runs first before this test!
 		
@@ -240,6 +240,74 @@ public class CashierTest extends TestCase
 		cash += 26.98;
 		assertEquals("Cashier's money should have increased by 26.98, but it didn't.", cashier.getMoney(),cash);
 
+	}
+	
+	public void testOneMarketOneCustomerScenario(){
+		double cash = cashier.getMoney();
+		customer.waiter = waiter;
+		customer.cashier = cashier;
+		
+		waiter.customer = customer;
+		waiter.cashier = cashier;
+		
+		//check preconditions
+		assertEquals("Cashier should have 0 orders in it. It doesn't.",cashier.pendingOrders.size(),0);
+		assertEquals("Cashier should have 0 owingCustomers in it. It doesn't.",cashier.owingCustomers.size(),0);
+		assertEquals("Cashier should have an empty event log before the Cashier's HereIsBill is called. Instead, the Cashier's event log reads: "
+						+ cashier.log.toString(), 0, cashier.log.size());
+		
+		//step 1 of the test
+		//msgFoodDelivered(Market, Item, Quantity)
+		cashier.msgFoodDelivered(market, "Steak", 1);//send the message from the market
+
+		//check postconditions for step 1 and preconditions for step 3
+		assertEquals("MockMarket should have an empty event log before the Cashier's scheduler is called. Instead, the MockWaiter's event log reads: "
+						+ market.log.toString(), 0, market.log.size());
+		
+		assertEquals("Cashier should have 1 bill in it. It doesn't.", cashier.payments.size(), 1);
+		
+		//step 2 of the test 
+		//msgHereIsBill(waiter,customer,choice,table)
+		cashier.msgHereIsBill(waiter, customer, "Steak", 0);
+		
+		//check post-conditions of step 2, pre-conditions of step 3
+		assertEquals("MockWaiter should have an empty event log before the Cashier's scheduler is called. Instead, the MockWaiter's event log reads: "
+				+ waiter.log.toString(), 0, waiter.log.size());
+		
+		assertEquals("MockCustomer should have an empty event log before the Cashier's scheduler is called. Instead, the MockCustomer's event log reads: "
+				+ customer.log.toString(), customer.log.size(), 0);
+		
+		assertTrue("Cashier should have \"Received bill for table 0\" in it, but it doesn't. Instead, the log reads: " + cashier.log.getLastLoggedEvent().toString(),cashier.log.containsString("Received bill for table 0"));
+		
+		assertEquals("Cashier should have 1 order in it. It doesn't.", cashier.pendingOrders.size(),1);
+		
+		//step 3 of the test 
+		assertTrue("Cashier's scheduler should have returned true (1 order to calculate), but didn't.", cashier.pickAndExecuteAnAction());
+		
+		//check post-conditions of step 3, pre-conditions of step 4
+		assertTrue("MockWaiter should have \"Received bill\" in its log, but it doesn't. Instead, the log reads: " + waiter.log.getLastLoggedEvent(), waiter.log.containsString("Received bill"));
+		assertTrue("MockWaiter should have \"Got money from customer\" in its log, but it doesn't. Instead, the log reads: " + waiter.log.getLastLoggedEvent(), waiter.log.containsString("Got money from customer"));
+		
+		assertTrue("MockCustomer should have \"Received HereIsYourTotal from cashier. Total = 15.99\" in its log, but it doesn't. Instead, the log reads: " + customer.log.getLastLoggedEvent(), customer.log.containsString("Received HereIsYourTotal from cashier. Total = 15.99"));
+		assertTrue("MockCustomer should have \"Received HereIsYourChange from cashier. Change = 0.01\" in its log, but it doesn't. Instead, the log reads: " + customer.log.getLastLoggedEvent(), customer.log.containsString("Received HereIsYourChange from cashier. Change = 0.01"));
+		
+		assertEquals("Cashier should have no more pending orders, but it does.", cashier.pendingOrders.size(), 0);
+		assertTrue("Cashier should have \"Got money from waiter\" in its log, but it doesn't. Instead, it reads: " + cashier.log.getLastLoggedEvent(), cashier.log.containsString("Got money from waiter"));
+		assertEquals("Cashier should have no more owingCustomers, but it doesn't.", cashier.owingCustomers.size(), 0);
+		cash += 15.99;
+		assertEquals("Cashier should have 15.99 more in its money, but it doesn't.", cashier.getMoney(), cash);
+		
+		//step 4 of the test
+		assertTrue("Cashier's scheduler should have returned true (1 bill to calculate), but didn't.", cashier.pickAndExecuteAnAction());
+		
+		//check post-conditions of step 2
+		assertEquals("Cashier should have 0 bills in it. It doesn't.", cashier.payments.size(),0);
+		
+		cash -= 15.99;
+		
+		assertEquals("Cashier's money should have decreased by 15.99. It didn't.", cashier.getMoney(),cash);
+		
+		assertTrue("MockMarket should have logged \"Received cash\", but it reads " + market.log.getLastLoggedEvent().toString(),market.log.containsString("Received cash"));
 	}
 	
 }
